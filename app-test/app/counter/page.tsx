da5a0d4b86d4232a53704d9178e232bd27edba7c
huzaifaghazali <huzaifaghazali@gmail.com>
2025-01-14 19:33:28 +0500

    Add counter page and component

---
+++ app-test/app/counter/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Counter from '@/components/Counter';
+
+const CounterPage = () => {
+  return (
+    <section>
+      <h1 className='text-6xl mb-16'>Page Content</h1>
+      <Counter />
+    </section>
+  );
+};
+
+export default CounterPage;
