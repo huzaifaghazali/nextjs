cf17125ab76b911e75d740e4d51bdebab6c7f2b9
huzaifaghazali <huzaifaghazali@gmail.com>
2025-01-27 22:41:24 +0500

    Add delete button ui and functionality

--- app-test/utils/actions.ts
+++ app-test/utils/actions.ts
@@ -2,7 +2,7 @@
 
 import { readFile, writeFile } from 'fs/promises';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 
 type User = {
   id: string;
@@ -12,7 +12,7 @@ type User = {
 
 export const createUser = async (prevState: any, formData: FormData) => {
   'use server';
-  
+
   await new Promise((resolve) => setTimeout(resolve, 3000));
   const firstName = formData.get('firstName') as string;
   const lastName = formData.get('lastName') as string;
@@ -38,4 +38,22 @@ const saveUser = async (user: User) => {
   const users = await fetchUsers();
   users.push(user);
   await writeFile('users.json', JSON.stringify(users));
+};
+
+export const deleteUser = async (formData: FormData) => {
+  const id = formData.get('id') as string;
+  const users = await fetchUsers();
+  const updatedUsers = users.filter((user: User) => user.id !== id);
+  await writeFile('users.json', JSON.stringify(updatedUsers));
+  revalidatePath('/actions');
+};
+
+export const removeUser = async (id: string, formData: FormData) => {
+  const name = formData.get('name') as string;
+  // console.log(name);
+
+  const users = await fetchUsers();
+  const updatedUsers = users.filter((user) => user.id !== id);
+  await writeFile('users.json', JSON.stringify(updatedUsers));
+  revalidatePath('/actions');
 };
